c745c8f00ee665da2da856fdcf515ded20d91b70
Edwell John Cotejar <ejcotejar@gmail.com>
2024-11-05 10:46:03 +0800

    Mine

--- src/main/java/org/example/SQLQueries/SQLOrder.java
+++ src/main/java/org/example/SQLQueries/SQLOrder.java
@@ -19,10 +19,7 @@ public class SQLOrder {
             preparedStatement.setInt(2, Meal_Quantity);
             preparedStatement.setString(3, Date);
 
-            int rowsAffected = preparedStatement.executeUpdate();
-            if (rowsAffected > 0) {
-                System.out.println("Order added successfully!");
-            }
+            System.out.println("Order added successfully!");
 
         } catch (SQLException e) {
             e.printStackTrace();
@@ -31,5 +28,20 @@ public class SQLOrder {
 
     public static void editOrder(int Order_Id, int Meal_Id, int Meal_Quantity, String Date) {
         String updateSQL = "UPDATE ORDERS SET Meal_Id = ?, Meal_Quantity = ?, Date = ? WHERE Order_Id = ?";
+
+        try (Connection connection = DriverManager.getConnection(DB_URL);
+            PreparedStatement preparedStatement = connection.prepareStatement(updateSQL)) {
+
+            preparedStatement.setInt(1, Meal_Id);
+            preparedStatement.setInt(2, Meal_Quantity);
+            preparedStatement.setString(3, Date);
+
+            System.out.println("Order edit successful.");
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
+
+
 }
