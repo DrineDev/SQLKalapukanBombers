0612d292d0dc2867c3ef087b1ddb4d61ebf49121
Edwell John Cotejar <ejcotejar@gmail.com>
2024-11-05 09:39:58 +0800

    small changes

--- src/main/java/org/example/SQLQueries/SQLInventory.java
+++ src/main/java/org/example/SQLQueries/SQLInventory.java
@@ -1,9 +1,7 @@
 package org.example.SQLQueries;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import javax.swing.*;
+import java.sql.*;
 
 public class SQLInventory {
 
@@ -22,6 +20,49 @@ public class SQLInventory {
             preparedStatement.executeUpdate();
 
             System.out.println("Meal added to inventory.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void editInventory(int Meal_ID, int Price, int Quantity_Available) {
+        String updateSQL = "UPDATE INVENTORY SET Price = ?, Quantity_Available = ? WHERE Meal_ID = ?";
+        try (Connection connection = DriverManager.getConnection(DB_URL);
+             PreparedStatement preparedStatement = connection.prepareStatement(updateSQL)) {
+            preparedStatement.setInt(1, Inventory_ID);
+            preparedStatement.setInt(2, Meal_ID);
+            preparedStatement.setInt(3, Quantity_Available);
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void deleteInventory(int id) {
+        String deleteSQL = "DELETE FROM INVENTORY WHERE Inventory_ID = ?";
+        try (Connection connection = DriverManager.getConnection(DB_URL);
+             PreparedStatement preparedStatement = connection.prepareStatement(deleteSQL)) {
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void listInventory() {
+        String query = "SELECT * FROM INVENTORY";
+        StringBuilder userList = new StringBuilder("Users:\n");
+        try (Connection connection = DriverManager.getConnection(DB_URL);
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(query)) {
+
+            while (resultSet.next()) {
+                userList.append("Inventory ID: ").append(resultSet.getInt("Inventory_ID"))
+                        .append(", Meal ID: ").append(resultSet.getString("Meal_ID"))
+                        .append(", Quantity Available: ").append(resultSet.getString("Quantity_Available")).append("\n");
+            }
+            JOptionPane.showMessageDialog(null, userList.toString());
         } catch (SQLException e) {
             e.printStackTrace();
         }
