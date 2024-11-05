a3ce091e6e7bb71fdf05dc41ec270cd2833a2123
Edwell John Cotejar <ejcotejar@gmail.com>
2024-11-05 08:50:34 +0800

    edit

--- src/main/java/org/example/SQLQueries/ExecuteQueries.java
+++ src/main/java/org/example/SQLQueries/ExecuteQueries.java
@@ -3,7 +3,7 @@ package org.example.SQLQueries;
 public class ExecuteQueries {
 
     public static void main(String[] args) {
-
+        SQLUser.addUser("admin", "123456789", "Manager");
     }
 
 }
