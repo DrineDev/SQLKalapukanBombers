f4e67e5d4d31bd5ddd69a50d2720bf243d8ae238
Edwell John Cotejar <ejcotejar@gmail.com>
2024-11-05 09:33:19 +0800

    Small

--- src/main/java/org/example/SQLQueries/SQLUser.java
+++ src/main/java/org/example/SQLQueries/SQLUser.java
@@ -1,5 +1,6 @@
 package org.example.SQLQueries;
 
+import javax.swing.*;
 import java.sql.*;
 
 public class SQLUser {
@@ -25,6 +26,54 @@ public class SQLUser {
             e.printStackTrace();
         }
     }
+
+    private static void editUser(int id, String username, String password, String role) {
+        String updateSQL = "UPDATE Users SET username = ?, password = ?, role = ? WHERE id = ?";
+        try (Connection connection = DriverManager.getConnection(DB_URL);
+             PreparedStatement preparedStatement = connection.prepareStatement(updateSQL)) {
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, password);
+            preparedStatement.setString(3, role);
+            preparedStatement.setInt(4, id);
+            preparedStatement.executeUpdate();
+
+            System.out.println("User edit successful.");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void deleteUser(int id) {
+        String deleteSQL = "DELETE FROM Users WHERE id = ?";
+        try (Connection connection = DriverManager.getConnection(DB_URL);
+             PreparedStatement preparedStatement = connection.prepareStatement(deleteSQL)) {
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
+
+            System.out.println("User delete successful.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+//    public static void listUsers() {
+//        String query = "SELECT * FROM Users";
+//        StringBuilder userList = new StringBuilder("Users:\n");
+//        try (Connection connection = DriverManager.getConnection(DB_URL);
+//             Statement statement = connection.createStatement();
+//             ResultSet resultSet = statement.executeQuery(query)) {
+//
+//            while (resultSet.next()) {
+//                userList.append("ID: ").append(resultSet.getInt("id"))
+//                        .append(", Username: ").append(resultSet.getString("username"))
+//                        .append(", Role: ").append(resultSet.getString("role")).append("\n");
+//            }
+//            JOptionPane.showMessageDialog(null, userList.toString());
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//    }
 
     public static boolean authenticateUser(String username, String password) {
         String query = "SELECT * FROM USERS WHERE username = ? AND password = ?";
