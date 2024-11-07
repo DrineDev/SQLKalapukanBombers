3b58f29f067049d1777503fe10c97289655ee653
wis <ejcotejar@gmail.com>
2024-11-07 22:12:15 +0800

    AddFood modified

--- src/main/java/org/example/AddFood.java
+++ src/main/java/org/example/AddFood.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
@@ -21,9 +20,6 @@ import javax.swing.JWindow;
 import javax.swing.Timer;
 
 import org.example.SQLQueries.SQLMeal;
-
-// TODO: Pick final AddFood function
-// TODO: Refactor
 
 public class AddFood extends JPanel {
   private ImageIcon foodImage;
@@ -164,10 +160,9 @@ public class AddFood extends JPanel {
     bottomLabel.add(orderButton);
     bottomArea.add(bottomLabel);
     this.add(bottomArea);
+    }
 
-  }
-  private void showImageFrame(String imagePath)
-    {
+    private void showImageFrame(String imagePath) {
         ImageIcon imageIcon = new ImageIcon(imagePath);
         Image image = imageIcon.getImage();
 
@@ -222,6 +217,7 @@ public class AddFood extends JPanel {
         });
         fadeInTimer.start();
     }
+
     private void startFadeOut(JWindow imageWindow)
     {
         Timer fadeOutTimer = new Timer(20, null);
@@ -244,88 +240,4 @@ public class AddFood extends JPanel {
         });
         fadeOutTimer.start();
     }
-    
-  public AddFood(ImageIcon foodImage, ImageIcon hoverImage) {
-    this.foodImage = foodImage;
-    this.hoverImage = hoverImage;
-
-    this.setPreferredSize(new Dimension(300, 300));
-    this.setLayout(null);
-
-    // Food background label
-    JLabel foodBg = new JLabel();
-    foodBg.setBounds(0, 0, 300, 250);
-    foodBg.setIcon(foodImage);
-
-    // Nutrition fact panel
-    JPanel nutritionPanel = new JPanel();
-    nutritionPanel.setBounds(0, 0, 300, 250);
-    nutritionPanel.setBackground(new Color(255, 255, 255, 200)); // Semi-transparent background
-    nutritionPanel.setVisible(false);
-
-    foodBg.addMouseListener(new MouseAdapter() {
-      @Override
-      public void mouseEntered(MouseEvent evt) {
-        if (hoverTimer != null && hoverTimer.isRunning()) {
-          hoverTimer.stop();
-        }
-        hoverTimer = new Timer(HOVER_DELAY, e -> foodBg.setIcon(hoverImage));
-        hoverTimer.setRepeats(false);
-        hoverTimer.start();
-      }
-
-      @Override
-      public void mouseExited(MouseEvent evt) {
-        if (hoverTimer != null && hoverTimer.isRunning()) {
-          hoverTimer.stop(); // Stop the timer if it's running
-        }
-        foodBg.setIcon(foodImage);
-      }
-    });
-
-    ImageIcon bottom = new ImageIcon("pics/Rectangle 16.png");
-    JLabel bottomPic = new JLabel(bottom);
-    JPanel bottomArea = new JPanel();
-    bottomArea.setBounds(0,250,300,50);
-    bottomArea.add(bottomPic);
-
-    ImageIcon incre = new ImageIcon("pics/less.png");
-    JButton increment = new JButton(incre);
-    increment.setBounds(0, 280, 11, 14);
-    increment.setBorderPainted(false);
-    increment.setFocusPainted(false);
-    increment.setContentAreaFilled(false);
-
-    ImageIcon decre = new ImageIcon("pics/more.png");
-    JButton decrement = new JButton(decre);
-    decrement.setBounds(50, 280, 11, 14);
-    decrement.setBorderPainted(false);
-    decrement.setFocusPainted(false);
-    decrement.setContentAreaFilled(false);
-
-    ImageIcon order = new ImageIcon("pics/order button.png");
-    JButton orderButton = new JButton(order);
-    orderButton.setBounds(200, 260, 80, 25);
-    orderButton.setBorderPainted(false);
-    orderButton.setFocusPainted(false);
-    orderButton.setContentAreaFilled(false);
-
-    JLabel amountTextField = new JLabel();
-    ImageIcon amountField = new ImageIcon();
-    int x = 0;
-    amountTextField.setText("" + x);
-
-    this.add(foodBg);
-    bottomArea.add(increment);
-    bottomArea.add(amountTextField);
-    bottomArea.add(decrement);
-    bottomArea.add(orderButton);
-    this.add(bottomArea);
-//    this.add(decrement, 3);
-//    this.add(amountTextField, 3);
-//    this.add(decrement, 1);
-//    this.add(orderButton, 1);
-
-  }
-
 }
