731628b4c45ee7156a180724637dc4bfefecd26a
toot-a-loot <c202301056@iacademy.edu.ph>
2024-11-26 03:26:33 +0800

    yey

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,13 +9,5 @@ mvn exec:java -Dexec.mainClass=org.example.Main
 public class Main {
     public static void main(String[] args) {
       SwingUtilities.invokeLater(LoginPage::new);
-    // SwingUtilities.invokeLater(InventoryCRUDEmployee::new);
-        // SwingUtilities.invokeLater(MainFrameEmployee::new);
-      // SwingUtilities.invokeLater(MainFrameManager::new);
-//        SwingUtilities.invokeLater(InventoryCRUD::new);
-    //  SwingUtilities.invokeLater(UserCRUD::new);
-        //  SwingUtilities.invokeLater(OrderCrud::new);
-
-
     }
 }
